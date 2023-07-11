5aa67e683d3156cfffa2df3cdd6e33ec1f7a0404
Khan-Syeedul-Islam <124359441+Khan-Syeedul-Islam@users.noreply.github.com>
2023-07-11 22:21:40 +0600

    Add files via upload

---
+++ Ostad-assigment/variables.js
@@ -0,0 +1,10 @@
+let age = 26;
+let names = "Syeed";
+let isStudent = true;
+let birthYear= '11/03/1997';
+console.log("My name is " + names + " and I am " + age + " years old.");
+console.log("I am a student: " + isStudent + ".");
+console.log("I was born in " + birthYear + ".");
+
+
+
